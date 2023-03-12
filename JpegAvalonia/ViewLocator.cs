1d730de62a001c1bd96d06943f07a3d7a211af3a
arnirichard <arni@arnirichard.dk>
2023-03-12 18:00:36 +0700

    Upgrade to Avalonia 11

--- JpegAvalonia/ViewLocator.cs
+++ JpegAvalonia/ViewLocator.cs
@@ -7,9 +7,9 @@ namespace JpegAvalonia
 {
     public class ViewLocator : IDataTemplate
     {
-        public IControl Build(object data)
+        public Control? Build(object? param)
         {
-            var name = data.GetType().FullName!.Replace("ViewModel", "View");
+            var name = param?.GetType().FullName!.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
             if (type != null)
@@ -22,7 +22,7 @@ namespace JpegAvalonia
             }
         }
 
-        public bool Match(object data)
+        public bool Match(object? data)
         {
             return data is ViewModelBase;
         }
--- JpegAvaloniaAsync/ViewLocator.cs
+++ JpegAvaloniaAsync/ViewLocator.cs
@@ -7,9 +7,9 @@ namespace JpegAvaloniaAsync
 {
     public class ViewLocator : IDataTemplate
     {
-        public IControl Build(object data)
+        public Control? Build(object? param)
         {
-            var name = data.GetType().FullName!.Replace("ViewModel", "View");
+            var name = param?.GetType().FullName!.Replace("ViewModel", "View");
             var type = Type.GetType(name);
 
             if (type != null)
@@ -22,7 +22,7 @@ namespace JpegAvaloniaAsync
             }
         }
 
-        public bool Match(object data)
+        public bool Match(object? data)
         {
             return data is ViewModelBase;
         }
